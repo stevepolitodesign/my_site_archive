9c77d735ef23e665ac11946794b8337e45012a04
Steve Polito <info@stevepolitodesign.com>
2020-10-24 09:16:37 -0400

    Install bullet gem.

--- Gemfile
+++ Gemfile
@@ -53,4 +53,5 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+gem "bullet", "~> 6.1"
 gem "strong_migrations", "~> 0.7.1"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # https://github.com/flyerhzm/bullet#configuration
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+  end
 end
