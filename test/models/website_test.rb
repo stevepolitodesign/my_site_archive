c4f04325f2df377d4b927201333463ebf2dc90d9
Steve Polito <info@stevepolitodesign.com>
2020-10-26 07:25:30 -0400

    Create blank tests.

--- test/models/website_test.rb
+++ test/models/website_test.rb
@@ -4,4 +4,20 @@ class WebsiteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should be valid" do
+    skip
+  end
+
+  test "should have a title" do
+    skip
+  end
+
+  test "should have a url" do
+    skip
+  end
+
+  test "should only save subdomain and domain as url" do
+    skip
+  end
 end
