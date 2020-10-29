70cba82d778c7cf85c932a4f551cd6a1ba8dc557
Steve Polito <info@stevepolitodesign.com>
2020-10-29 07:50:04 -0400

    Build call method.

--- app/services/dns_record_creator.rb
+++ app/services/dns_record_creator.rb
@@ -1,11 +1,38 @@
 require "resolv"
 
 class DnsRecordCreator
-    def initialize(resource)
-        @resource = resource
+    def initialize(zone_file, resource)
+        @zone_file      = zone_file
+        @resource       = resource 
+        @record_type    = @resource.class.name.split('::').last.downcase
     end
 
     def call
-        
+        @dns_record = @zone_file.dns_records.create(record_type: @record_type, content: set_content )
+        if @dns_record.save
+            OpenStruct.new({success?: true, payload: @dns_record})
+        else
+            OpenStruct.new({success?: false, error: @dns_record.errors.full_messages.to_sentence})
+        end
     end
+
+    private
+
+        def set_content
+            @contet = nil
+            case @record_type
+            when "cname"
+                @contet = @resource.name.to_s
+            when "a"
+                @contet = @resource.name.to_s
+            when "aaaa"
+                @contet = @resource.name.to_s
+            when "mx"
+                @contet = @resource.exchange.to_s
+            when "ns"
+                @contet = @resource.name.to_s
+            when "txts"
+                @contet = @resource.strings.to_s
+            end
+        end
 end
