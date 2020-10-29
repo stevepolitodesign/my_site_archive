3b677448e7287e713854c19f2c4b71ff92ce01e0
Steve Polito <info@stevepolitodesign.com>
2020-10-28 22:05:51 -0400

    Create websites_controller#new and websites_controller#create

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,2 +1,21 @@
 class WebsitesController < ApplicationController
+    def new
+        @website = Website.new
+    end
+
+    def create
+        @website = Website.create(website_params)
+        if @website.save
+            redirect_to @website, notice: "Website created."
+            CreateZoneFileJob.perform_later(@website.id)
+        else
+            render "new"
+        end
+    end
+
+    private
+
+        def website_params
+            params.require(:website).permit(:title, :url)
+        end
 end
