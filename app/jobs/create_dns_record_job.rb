0cc4d47ff26162c8614ace988344c9f9ae530ee7
Steve Polito <info@stevepolitodesign.com>
2020-10-29 19:35:53 -0400

    Begin to create job for creating individual DNS records.

---
+++ app/jobs/create_dns_record_job.rb
@@ -0,0 +1,9 @@
+class CreateDnsRecordJob < ApplicationJob
+  queue_as :default
+
+  def perform(zone_file_id, resources)
+    @zone_file = ZoneFile.find_by(id: zone_file_id)
+    return if @zone_file.nil?
+    raise
+  end
+end
---
+++ test/jobs/create_dns_record_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CreateDnsRecordJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
