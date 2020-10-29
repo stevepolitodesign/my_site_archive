61cbd29f823f35381521bfd2bde8f3946b076675
Steve Polito <info@stevepolitodesign.com>
2020-10-29 07:37:57 -0400

    Update initialize method.

--- app/services/dns_record_creator.rb
+++ app/services/dns_record_creator.rb
@@ -1,12 +1,11 @@
 require "resolv"
 
 class DnsRecordCreator
-    def initialize(zone_file, record_type)
-        @zone_file  = zone_file
-        @website    = @zone_file.website
+    def initialize(resource)
+        @resource = resource
     end
 
     def call
-        # TODO: I need to call this once per record_type per result record_type
+        
     end
 end
