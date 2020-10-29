39e13f45738c0842298aebb9efef8226d32d8844
Steve Polito <info@stevepolitodesign.com>
2020-10-29 19:21:09 -0400

    Spelling.

--- app/jobs/create_dns_records_job.rb
+++ app/jobs/create_dns_records_job.rb
@@ -5,13 +5,13 @@ class CreateDnsRecordsJob < ApplicationJob
     @zone_file = ZoneFile.find_by(id: zone_file_id)
     return if @zone_file.nil?
     DnsRecord.record_types.keys.each do |record_type|
-      retreive_dns_records(@zone_file.website.url, record_type)
+      retrieve_dns_records(@zone_file.website.url, record_type)
     end
   end
 
   private
 
-    def retreive_dns_records(zone_file_id, address, record_type)
+    def retrieve_dns_records(zone_file_id, address, record_type)
       result = DnsRecordRetriever.new(address, record_type)
       if result.success?
         CreateDnsRecord.perform_later(zone_file_id, result)
