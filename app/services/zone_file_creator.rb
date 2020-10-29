3b0ec2ea08091e16b8c68b64ea3f58a0330dab6a
Steve Polito <info@stevepolitodesign.com>
2020-10-29 07:24:42 -0400

    Replace payload key with error key.

--- app/services/zone_file_creator.rb
+++ app/services/zone_file_creator.rb
@@ -8,7 +8,7 @@ class ZoneFileCreator
         if @zone_file.save
             OpenStruct.new({success?: true, payload: @zone_file})
         else
-            OpenStruct.new({success?: false, payload: @zone_file.errors.full_messages.to_sentence})
+            OpenStruct.new({success?: false, error: @zone_file.errors.full_messages.to_sentence})
         end
     end
 end
