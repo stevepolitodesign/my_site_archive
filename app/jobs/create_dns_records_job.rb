aa4cfdda064a03ec92029e1fe67909ea1057c7af
Steve Polito <info@stevepolitodesign.com>
2020-10-29 19:17:39 -0400

    Create job for queuing up the creation of Dns Records.

---
+++ app/jobs/create_dns_records_job.rb
@@ -0,0 +1,22 @@
+class CreateDnsRecordsJob < ApplicationJob
+  queue_as :default
+
+  def perform(zone_file_id)
+    @zone_file = ZoneFile.find_by(id: zone_file_id)
+    return if @zone_file.nil?
+    DnsRecord.record_types.keys.each do |record_type|
+      retreive_dns_records(@zone_file.website.url, record_type)
+    end
+  end
+
+  private
+
+    def retreive_dns_records(address, record_type)
+      result = DnsRecordRetriever.new(address, record_type)
+      if result.success?
+        CreateDnsRecord.perform_later(result)
+      else
+        return
+      end
+    end
+end
