b4edd70f1a4ea6d10f8055128753726bde8ae931
Steve Polito <info@stevepolitodesign.com>
2020-10-29 21:31:02 -0400

    render zone file and associated dns records on websites#show.

--- app/models/website.rb
+++ app/models/website.rb
@@ -3,6 +3,7 @@ require 'uri'
 class Website < ApplicationRecord
     has_many :webpages, dependent: :destroy
     has_many :zone_files, dependent: :destroy
+    has_one :latest_zone_file, -> { order('created_at') }, class_name: "ZoneFile"
 
     validates :title, :url, presence: true
     validates :url, url: true
