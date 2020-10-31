4d76a34ffd9da5b1155ac6b722ed4aa6b94ba8de
Steve Polito <info@stevepolitodesign.com>
2020-10-31 08:04:54 -0400

    create html_documents_creator and show page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :websites
+  resources :html_ducuments, only: [:show]
 end
