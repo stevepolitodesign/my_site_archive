d5d1c8023786f3a2cfe33d6a5946a3b7d873ae48
Steve Polito <info@stevepolitodesign.com>
2020-11-02 07:27:44 -0500

    Continue to build screenshot job.

--- app/jobs/create_screenshot_job.rb
+++ app/jobs/create_screenshot_job.rb
@@ -3,13 +3,22 @@ require 'capybara/dsl'
 class CreateScreenshotJob < ApplicationJob
   include Capybara::DSL
 
+  around_enqueue :configure_capybara
+
   queue_as :default
 
   def perform(webpage_id)
     @webpage = Webpage.find_by(id: webpage_id)
     return if @webpage.nil?
-    configure_capybara
-    page.save_screenshot
+    visit @webpage.url
+    # TODO: Make this a service object.
+    path = File.join Rails.root, 'tmp', 'screenshots'
+    FileUtils.mkdir_p(path) unless File.exist?(path)
+    file_path = page.save_screenshot(path)
+    @screenshot = @webpage.screenshots.build
+    @screenshot.image.attach(io: file_path)
+    @screenshot.save
+    # TODO: Delete file after save
   end
 
   private
@@ -17,6 +26,6 @@ class CreateScreenshotJob < ApplicationJob
     def configure_capybara
       Capybara.run_server = false
       Capybara.current_driver = :selenium_chrome_headless
-      Capybara.app_host = @wepage.url
-    end
+      Capybara.app_host = @webpage.url
+    end 
 end
