19324a3ae624c249df74ff13ebf671c8c79da86f
Steve Polito <info@stevepolitodesign.com>
2020-11-02 21:16:17 -0500

    create service object for capturing a screenshot.

--- app/jobs/create_screenshot_job.rb
+++ app/jobs/create_screenshot_job.rb
@@ -1,33 +1,18 @@
-require 'capybara/dsl'
-
 class CreateScreenshotJob < ApplicationJob
-  include Capybara::DSL
-
-  after_perform :configure_capybara
-
   queue_as :default
 
   def perform(webpage_id)
     @webpage = Webpage.find_by(id: webpage_id)
     return if @webpage.nil?
-    # TODO: Break these up into smaller methods.
-    visit @webpage.url
-    directory = File.join(Rails.root, 'tmp', 'screenshots')
-    Dir.mkdir(directory) unless File.directory?(directory)
-    file_name = "#{@webpage.url.parameterize}.png"
-    file_path = page.save_screenshot("#{directory}/#{@webpage.url.parameterize}.png")
-    # TODO: Make this a service object.
-    @screenshot = @webpage.screenshots.build
-    @screenshot.image.attach(io: File.open(file_path), filename: file_name)
-    @screenshot.save
-    # TODO: Delete file after save
+    result = ScreenshotCapturer.new(@webpage.url).call
+    if result.success?
+      # TODO: Make this a service object.
+      file_name = @webpage.url.parameterize
+      @screenshot = @webpage.screenshots.build
+      @screenshot.image.attach(io: File.open(result.payload), filename: file_name)
+      @screenshot.save
+    else
+      return
+    end
   end
-
-  private
-
-    def configure_capybara
-      Capybara.run_server = false
-      Capybara.current_driver = :selenium_chrome_headless
-      Capybara.app_host = @webpage.url
-    end 
 end
---
+++ app/services/screenshot_capturer.rb
@@ -0,0 +1,51 @@
+require 'capybara/dsl'
+
+class ScreenshotCapturer
+    include Capybara::DSL
+   
+    def initialize(url)
+        @url = url
+    end
+
+    def call
+        begin
+            configure_capybara
+            create_directory
+            visit_url
+            screenshot = take_screenshot(create_directory)
+            OpenStruct.new({ success?: true, payload: screenshot })
+        rescue => error
+            OpenStruct.new({ success?: false, error: error })
+        end
+    end
+
+    private
+
+        attr_reader :url
+
+        def configure_capybara
+            Capybara.run_server = false
+            Capybara.current_driver = :selenium_chrome_headless
+            Capybara.app_host = url
+        end
+        
+        def create_directory
+            directory = File.join(Rails.root, 'tmp', 'screenshots')
+            Dir.mkdir(directory) unless File.directory?(directory)
+            return directory
+        end
+
+        def visit_url
+            visit url
+        end
+
+        def take_screenshot(directory)
+            file_name = "#{url.parameterize}.png"
+            page.save_screenshot("#{directory}/#{url.parameterize}.png")
+        end
+
+        def delete_screenshot
+            # TODO
+        end
+        
+end
