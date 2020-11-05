2c2d72edefa5191ee32fc379e9be49c80032310b
Steve Polito <info@stevepolitodesign.com>
2020-11-04 21:27:02 -0500

    create homepage.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "static_pages#home"
   devise_for :users
   resources :html_ducuments, only: [:show]
   resources :websites do
