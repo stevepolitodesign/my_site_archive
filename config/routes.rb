27714cb27f68e20e248609b7e953095e19ae8d55
Steve Polito <info@stevepolitodesign.com>
2020-11-04 21:14:12 -0500

    generate user model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
   resources :html_ducuments, only: [:show]
   resources :websites do
     resources :webpages, only: [:show, :create] do
