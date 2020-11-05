3177e1c1c662c291b93f9ccc1060d6e85958ce0d
Steve Polito <info@stevepolitodesign.com>
2020-11-04 21:41:56 -0500

    write failing tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.build(
+      email: "user@example.com",
+      password: "password",
+      password_confirmation: "password",
+      confirmed_at: Time.zone.now
+    )
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "should destroy associated websites" do
+    @user.save
+    @website = @user.websites.create(title: "title", url: "http://www.example.com")
+    assert_difference("Website.count", -1) do
+      @user.destroy
+    end
+  end
 end
--- test/system/user_flows_test.rb
+++ test/system/user_flows_test.rb
@@ -15,4 +15,5 @@ class UserFlowsTest < ApplicationSystemTestCase
 
     assert_text "A message with a confirmation link has been sent to your email address. Please follow the link to activate your account."
   end
+
 end
