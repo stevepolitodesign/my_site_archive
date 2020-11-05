f2070e9b7f5217618a75cb46ca37771a348a8b9d
Steve Polito <info@stevepolitodesign.com>
2020-11-04 21:32:31 -0500

    write system test for signing up.

--- test/system/user_flows_test.rb
+++ test/system/user_flows_test.rb
@@ -1,14 +1,18 @@
 require "application_system_test_case"
 
 class UserFlowsTest < ApplicationSystemTestCase
+  include ActionMailer::TestHelper
+
   test "creating an account" do
     visit new_user_registration_path
     
-    fill_in "Email", with: "example@example.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_button "Sign up"
+    assert_emails 1 do
+      fill_in "Email", with: "example@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      click_button "Sign up"
+    end
 
-    take_screenshot
+    assert_text "A message with a confirmation link has been sent to your email address. Please follow the link to activate your account."
   end
 end
