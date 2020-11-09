6a2460c7bb20db59f157fb38c7e5f8d00aedcfc4
Steve Polito <info@stevepolitodesign.com>
2020-11-09 08:55:02 -0500

    Create simple subscription form.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,4 +3,11 @@ class SubscriptionsController < ApplicationController
 
     def new
     end
+
+    def create
+        current_user.update(processor: "stripe", card_token: params[:card_token])
+        current_user.subscribe(name: "Tier One: Monthly Subscription", plan: "price_1HkrqzIZ6Y8BQ2bm4CJfzmno", )
+        redirect_to root_path, notice: "Subscribed"
+    end
+
 end
