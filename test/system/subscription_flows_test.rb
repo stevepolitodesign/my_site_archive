4c90a689cb8eefcc4ef387778a9f9e2a6cc9d577
Steve Polito <info@stevepolitodesign.com>
2020-11-10 19:57:02 -0500

    Create pending tests.

---
+++ test/system/subscription_flows_test.rb
@@ -0,0 +1,23 @@
+require "application_system_test_case"
+
+class SubscriptionFlowsTest < ApplicationSystemTestCase
+  test "creating a subscription" do
+    skip
+  end
+
+  test "updating a subscription" do
+    skip
+  end
+
+  test "deleting a subscription" do
+    skip
+  end
+
+  test "canceling an account" do
+    skip
+  end
+
+  test "preventing suplicate subscriptions" do
+    skip
+  end
+end
