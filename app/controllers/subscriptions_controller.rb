faa8c5d0361fa226e61d893639849e15ed34de22
Steve Polito <info@stevepolitodesign.com>
2020-11-10 19:46:14 -0500

    Handle errors.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -10,7 +10,8 @@ class SubscriptionsController < ApplicationController
         # TODO: Set these values through a Plan model
         current_user.subscribe(name: params[:plan], plan: params[:plan])
         redirect_to root_path, notice: "Subscribed"
-        # TODO: Rescue 
+    rescue Pay::Error
+        redirect_to new_subscription_path, notice: "There was an error creating your subscription."
     end
 
     def edit
