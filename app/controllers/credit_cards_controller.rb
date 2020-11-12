371c39b5097e5ec669ce0f15905d781369aa2da9
Steve Polito <info@stevepolitodesign.com>
2020-11-11 21:34:40 -0500

    Allow user to update their credit card.

---
+++ app/controllers/credit_cards_controller.rb
@@ -0,0 +1,10 @@
+class CreditCardsController < ApplicationController
+    before_action :authenticate_user!
+
+    def update
+        current_user.update_card(params[:card_token])
+        redirect_to root_path, notice: "Credit card updated."
+    rescue Pay::Error
+        redirect_to subscription_path, alert: "There was an error updating your credit card."
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "static_pages#home"
   devise_for :users
+  resource :credit_card, only: [:update]
   resource :subscription
   resource :resume_subscription, only: [:update]
   resources :html_ducuments, only: [:show]
