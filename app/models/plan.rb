f769ec1e07c70bb681a7d9602ae6ed1b3cddc84f
Steve Polito <info@stevepolitodesign.com>
2020-11-11 19:12:21 -0500

    Render plans in subscription form.

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,9 +1,11 @@
 class Plan < ApplicationRecord
+    include ActionView::Helpers::NumberHelper
+
     validates :name, presence: true
 
     def formatted_name
-        if self.number_to_currency.present?
-            "#{self.name} (#{number_to_currency(self.price_in_cents)})"
+        if self.price_in_cents.present?
+            "#{self.name} (#{number_to_currency(self.price_in_cents / 100)})"
         else 
             "#{self.name} (#{number_to_currency(0)})"
         end
