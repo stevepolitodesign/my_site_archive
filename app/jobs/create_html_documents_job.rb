65f04c6b2caf694b8aff7d32b5af282bf7dbf339
Steve Polito <info@stevepolitodesign.com>
2020-11-15 12:57:48 -0500

    Notes.

--- app/jobs/create_html_documents_job.rb
+++ app/jobs/create_html_documents_job.rb
@@ -4,6 +4,7 @@ class CreateHtmlDocumentsJob < ApplicationJob
   before_perform :set_users
 
   def perform
+    # TODO: Consider querying for all Webpages where the Website User is subscribed.
     @users.find_in_batches(batch_size: 100) do |group|
       group.each do |user|
         user.capture_new_html_documents
--- app/jobs/create_zone_files_job.rb
+++ app/jobs/create_zone_files_job.rb
@@ -4,6 +4,7 @@ class CreateZoneFilesJob < ApplicationJob
   before_perform :set_users
 
   def perform
+    # TODO: Consider querying for all Websites where the User is subscribed. 
     @users.find_in_batches(batch_size: 100) do |group|
       group.each do |user|
         user.capture_new_zone_files
