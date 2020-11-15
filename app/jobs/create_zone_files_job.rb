72e7bf77d79af86d45536fe97470578e4a3bc35d
Steve Polito <info@stevepolitodesign.com>
2020-11-15 09:38:51 -0500

    Refactor.

--- app/jobs/create_zone_files_job.rb
+++ app/jobs/create_zone_files_job.rb
@@ -4,14 +4,9 @@ class CreateZoneFilesJob < ApplicationJob
   before_perform :set_users
 
   def perform
-    @users.each do |user|
-      if user.current_plan.present? && user.current_plan.job_schedule_frequency.present?
-        duration = user.current_plan.job_schedule_frequency
-        @websites = user.websites
-        @websites.each do |website|
-          @zone_file = website.latest_zone_file
-          CreateZoneFileJob.perform_later(website.id) if @zone_file.nil? || @zone_file.created_at >= 1.send(duration).from_now
-        end
+    @users.find_in_batches(batch_size: 100) do |group|
+      group.each do |user|
+        user.update_websites_zone_file
       end
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,20 @@ class User < ApplicationRecord
   def current_plan
     Plan.find_by(processor_id: self.subscription.processor_plan) unless self.subscription.nil?
   end
+
+  def current_plan_job_schedule_frequency
+    return if current_plan.nil?
+    self.current_plan.job_schedule_frequency
+  end
+
+  def update_websites_zone_file
+    return if self.current_plan_job_schedule_frequency.nil?
+    duration = current_plan_job_schedule_frequency
+    self.websites.each do |website|
+      @zone_file = website.latest_zone_file
+      if @zone_file.nil? || @zone_file.created_at >= 1.send(duration).from_now
+        CreateZoneFileJob.perform_later(website.id)
+      end
+    end
+  end
 end
