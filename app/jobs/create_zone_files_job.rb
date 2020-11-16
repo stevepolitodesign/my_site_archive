c058640ffc08848086016bf4536d8dd144b8f1c7
Steve Polito <info@stevepolitodesign.com>
2020-11-15 19:42:48 -0500

    Remove comments.

--- app/jobs/create_zone_files_job.rb
+++ app/jobs/create_zone_files_job.rb
@@ -2,7 +2,6 @@ class CreateZoneFilesJob < ApplicationJob
   queue_as :default
 
   def perform
-    # TODO: Consider querying for all Websites where the User is subscribed. 
     Website.with_active_subscribers.each do |website|
       website.capture_new_zone_file if website.should_capture_new_zone_file?
     end
