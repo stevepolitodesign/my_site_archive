2a0909592ecbabcb80b1aea7cd1005a9dc462bb1
Steve Polito <info@stevepolitodesign.com>
2020-11-16 19:34:45 -0500

    Test create_html_documents_job.

--- test/jobs/create_html_documents_job_test.rb
+++ test/jobs/create_html_documents_job_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 class CreateHtmlDocumentsJobTest < ActiveJob::TestCase
   # TODO: Use VCR
-  # TODO: Test
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create screenshots" do
+    assert_equal 2, HtmlDocument.count
+    
+    travel_to 1.week.from_now
+    perform_enqueued_jobs do
+      CreateHtmlDocumentsJob.perform_now
+    end
+    assert_equal 4, HtmlDocument.count
+  end 
 end
