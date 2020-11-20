adf909eed69a81ca7660215c1f2e9a09a2270309
Steve Polito <info@stevepolitodesign.com>
2020-11-20 06:43:05 -0500

    Notes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # TODO: Redirec to subscriptio#new on new user creation
+  # TODO: Update Stripe URLs 
   root to: "static_pages#home"
   devise_for :users, controllers: {
     registrations: 'users/registrations'
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   resource :resume_subscription, only: [:update]
   resources :websites do
     resources :webpages, only: [:show, :edit, :create, :update, :destroy] do
-      resources :html_documents, only: [:show]
+      resources :html_documents, only: [:index, :show]
     end
   end
 end
