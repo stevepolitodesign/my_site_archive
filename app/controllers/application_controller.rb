b77b9c0cde86a06b2c657016281408908e717351
Steve Polito <info@stevepolitodesign.com>
2020-11-21 09:07:43 -0500

    Whitelist specific controllers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,34 @@
 class ApplicationController < ActionController::Base
     include Pundit
-    after_action :verify_authorized
+    after_action :verify_authorized, unless: :exempt_controllers
 
     rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
     private
 
+        def credit_cards_controller?
+            controller_name == "credit_cards"
+        end 
+
+        def exempt_controllers
+            credit_cards_controller? || devise_controller? || resume_subscriptions_controller? || static_pages_controller? || subscriptions_controller?
+        end
+ 
+        def resume_subscriptions_controller?
+            controller_name == "resume_subscriptions"
+        end         
+        
+        def static_pages_controller?
+            controller_name == "static_pages"
+        end
+        
+        def subscriptions_controller?
+            controller_name == "subscriptions"
+        end
+        
         def user_not_authorized
             flash[:alert] = "You are not authorized to perform this action."
             redirect_to(request.referrer || root_path)
         end
+        
 end
