011288fb691435c6666c7689e02f367d568186e4
Steve Polito <info@stevepolitodesign.com>
2020-11-22 07:48:27 -0500

    Create authenticated root.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # TODO: Redirec to subscriptio#new on new user creation
   # TODO: Update Stripe URLs 
-  root to: "static_pages#home"
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  unauthenticated do
+    root "static_pages#home"
+  end
+  authenticated :user do
+    root "websites#index", as: :authenticated_root
+  end
   resource :credit_card, only: [:update]
   resource :subscription
   resource :resume_subscription, only: [:update]
