d7c8967992297acaca8806429338e40dd9cf2f87
Steve Polito <info@stevepolitodesign.com>
2020-11-21 19:19:32 -0500

    Continue to style app.
    * Focus on subscription pages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,19 +14,29 @@ class User < ApplicationRecord
   scope :with_active_subscriptions, -> { joins(:subscriptions).where({ pay_subscriptions: { status: "active" } }).distinct }
   scope :with_websites, -> { joins(:websites).distinct }
 
+  def card_on_file
+    if has_card?
+      "#{card_type} #{card_last4} expiring on #{card_exp_month}/#{card_exp_year}"
+    end
+  end
+  
+  def current_plan
+    Plan.find_by(processor_id: self.subscription.processor_plan) unless self.subscription.nil?
+  end
+  
+  def current_plan_job_schedule_frequency
+    return if current_plan.nil?
+    self.current_plan.job_schedule_frequency
+  end
+
   def destroy
     self.subscription.cancel_now! if self.subscribed?
     rescue Pay::Error
     super
   end
 
-  def current_plan
-    Plan.find_by(processor_id: self.subscription.processor_plan) unless self.subscription.nil?
-  end
-
-  def current_plan_job_schedule_frequency
-    return if current_plan.nil?
-    self.current_plan.job_schedule_frequency
+  def has_card?
+    card_type.present? && card_last4.present? && card_exp_month.present? && card_exp_year.present?
   end
 
 end
