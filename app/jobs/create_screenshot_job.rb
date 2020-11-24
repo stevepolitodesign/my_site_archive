322549ef586cb0949d0e61583718df62a7c29953
Steve Polito <info@stevepolitodesign.com>
2020-11-23 21:26:02 -0500

    Refactor job for legibility.

--- app/jobs/create_screenshot_job.rb
+++ app/jobs/create_screenshot_job.rb
@@ -1,20 +1,50 @@
 class CreateScreenshotJob < ApplicationJob
   queue_as :default
 
+  before_perform :set_webpage
+
   def perform(webpage_id)
-    @webpage = Webpage.find_by(id: webpage_id)
     return if @webpage.nil?
-    result = ScreenshotCapturer.new(@webpage.url).call
-    if result.success?
-      screenshot = result.payload
-      result = ScreenshotCreator.new(webpage_id, screenshot).call
-      if result.success?
-        @screenshot = result.payload
-        CreateHtmlDocumentJob.perform_later(@screenshot.id)
-      end
-      TmpFileRemover.new(screenshot).call
-    else
-      return
-    end
+    capture_and_create_screenshot_and_html_document
   end
+
+  private
+  
+    def capture_screenshot
+      ScreenshotCapturer.new(@webpage.url).call
+    end
+
+    def capture_and_create_screenshot
+      result = capture_screenshot
+      if result.success?
+        screenshot  = result.payload 
+        result      = create_screenshot(screenshot)
+        TmpFileRemover.new(screenshot).call
+        result
+      else
+        return 
+      end
+    end
+    
+    def create_html_document(screenshot_id)
+      CreateHtmlDocumentJob.perform_later(screenshot_id)
+    end
+    
+    def create_screenshot(screenshot)
+      ScreenshotCreator.new(arguments.first, screenshot).call
+    end
+    
+    def capture_and_create_screenshot_and_html_document
+      result = capture_and_create_screenshot
+      if result.success?
+        create_html_document(result.payload.id)
+      else
+        return
+      end
+    end
+
+    def set_webpage
+      @webpage = Webpage.find_by(id: arguments.first)
+    end
+
 end
