2e1317fa8e6b13d79a273af3bf80badc79be0453
Steve Polito <info@stevepolitodesign.com>
2020-11-26 17:53:45 -0500

    Style devise form errors.

--- test/system/user_flows_test.rb
+++ test/system/user_flows_test.rb
@@ -3,6 +3,10 @@ require "application_system_test_case"
 class UserFlowsTest < ApplicationSystemTestCase
   include ActionMailer::TestHelper
 
+  def setup
+    @user = users(:unsubscribed_user)
+  end
+
   test "creating an account" do
     visit new_user_registration_path
     
@@ -15,10 +19,31 @@ class UserFlowsTest < ApplicationSystemTestCase
     end
 
     assert_text "A message with a confirmation link has been sent to your email address. Please follow the link to activate your account."
+
+    take_screenshot
   end
 
   test "deleting an account with no subscription" do
-    skip
+    sign_in @user
+
+    visit edit_user_registration_path
+
+    accept_confirm do
+      click_button "Cancel my account and subscription"
+    end
+
+    assert_text "Bye! Your account has been successfully cancelled"
+
+    take_screenshot
+  end
+
+  test "rendering form errors" do
+    sign_in @user
+
+    visit edit_user_registration_path
+    click_button "Update"
+
+    take_screenshot
   end
 
 end
