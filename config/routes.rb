ee9dc73ec3f23957ae036cead4d95736b2c8f82c
Steve Polito <info@stevepolitodesign.com>
2020-11-26 20:14:01 -0500

    Remove notes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # TODO: Redirec to subscriptio#new on new user creation
   # TODO: Update Stripe URLs 
   devise_for :users, controllers: {
     registrations: 'users/registrations'
