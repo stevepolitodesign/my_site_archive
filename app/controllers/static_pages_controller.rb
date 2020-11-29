ab244ffe5914d26e7229645b5a1ea00e297df596
Steve Polito <info@stevepolitodesign.com>
2020-11-29 07:54:37 -0500

    Create pricing partial.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,10 @@ class StaticPagesController < ApplicationController
     # TODO: Add Privcy and Terms 
     def home
     end
+
+    def pricing
+    end
+
+    def faqs
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,5 @@ Rails.application.routes.draw do
     end
     resources :zone_files, only: [:index, :show]
   end
+  get "pricing", to: "static_pages#pricing", as: "pricing"
 end
