c960aa22ba2a0dcd82c3e26e9936f92964a8333c
Steve Polito <info@stevepolitodesign.com>
2020-12-01 21:54:39 -0500

    Enable Sidekiq dashboard for admins.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # TODO: Update Stripe URLs 
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
