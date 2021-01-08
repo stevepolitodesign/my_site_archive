b2b5c2601b5393dc3d719e49629eb65d703c8b13
Steve Polito <info@stevepolitodesign.com>
2021-01-07 19:51:28 -0500

    Build PostsController.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,56 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, except: :show
+
+    def index
+        @posts = Post.all.order(created_at: :desc)
+    end
+
+    def show
+        authorize @post
+    end
+
+    def new
+        @post = Post.new
+        authorize @post
+    end
+
+    def create
+        @post = Post.create(post_params)
+        authorize @post
+        if @post.save
+            redirect_to @post, notice: "Post created."
+        else
+            render "new" 
+        end
+    end
+
+    def edit
+        authorize @post
+    end
+
+    def update
+        authorize @post
+        if @post.update(post_params)
+            redirect_to @post, notice: "Post updated."
+        else
+            render "edit"
+        end
+    end
+
+    def destroy
+        authorize @post
+        @post.destroy
+        redirect_to posts_path, notice: "Post deleted."
+    end
+    
+    private
+
+        def post_params
+            params.require(:post).permit(:title, :meta_description, :status, :content)
+        end
+
+        def set_post
+            @post = Post.friendly.find(params[:id])
+        end
 end
