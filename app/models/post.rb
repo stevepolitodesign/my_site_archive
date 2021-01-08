1b583cc986e463119e399c422cf35856a5ed7055
Steve Polito <info@stevepolitodesign.com>
2021-01-07 19:39:00 -0500

    Add enum and scope.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     extend FriendlyId
+    enum status: [:draft, :published]
+    scope :published, -> { where(status: :published) }
     friendly_id :title, use: :slugged
-
     has_rich_text :content
 end
