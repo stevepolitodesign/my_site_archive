5670f7f0154d2a006a23ce8b9bc5e70065528fb7
Steve Polito <info@stevepolitodesign.com>
2021-01-09 09:58:42 -0500

    Add policy scope to posts#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     before_action :authenticate_user!, except: [:show, :index]
 
     def index
-        @posts = Post.all.includes([:rich_text_content]).order(created_at: :desc)
+        @posts = policy_scope Post.all.includes([:rich_text_content]).order(created_at: :desc)
     end
 
     def show
