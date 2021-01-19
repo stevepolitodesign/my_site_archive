2b1d1f74796c2eb8e7bef7bd87f240fcbb1ea900
Steve Polito Design <stevepolitodesign@users.noreply.github.com>
2021-01-18 21:02:31 -0500

    Add features page and description. (#63)
    Co-authored-by: Steve Polito <info@stevepolitodesign.com>

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,16 @@
 class StaticPagesController < ApplicationController
     def home
+        set_meta_tags  description: "My Site Archive captures screenshots, downloads source code, and monitors DNS records helping you keep track of changes on your websites without needing to restore from a backup. Think of it like the Wayback Machine on steroids."
     end
 
     def pricing
     end
 
     def faqs
+    end
+
+    def features
+        set_meta_tags  description: "My Site Archive captures screenshots, downloads source code, and monitors DNS records helping you keep track of changes on your websites without needing to restore from a backup. Think of it like the Wayback Machine on steroids."
     end
 
     def terms_of_use
