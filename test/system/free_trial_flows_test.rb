d7de108885ec7ad3ba07e20caea6d55f74876123
Steve Polito <info@stevepolitodesign.com>
2021-02-01 20:21:33 -0500

    WIP: Pending system tests.

---
+++ test/system/free_trial_flows_test.rb
@@ -0,0 +1,16 @@
+require "application_system_test_case"
+
+class FreeTrialFlowsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit free_trial_flows_url
+  #
+  #   assert_selector "h1", text: "FreeTrialFlow"
+  # end
+  test "access to websites while on free trial" do
+    skip
+  end
+
+  test "access to websites while not on free trial" do
+    skip
+  end  
+end
