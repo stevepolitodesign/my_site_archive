2a7f27ad2a239ed8147c2ee2d8cd46308e3768bc
Steve Polito <info@stevepolitodesign.com>
2021-02-01 20:43:38 -0500

    Test free trial interface.

--- test/system/free_trial_flows_test.rb
+++ test/system/free_trial_flows_test.rb
@@ -1,20 +1,30 @@
 require "application_system_test_case"
 
 class FreeTrialFlowsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit free_trial_flows_url
-  #
-  #   assert_selector "h1", text: "FreeTrialFlow"
-  # end
-  test "access to websites while on free trial" do
-    skip
+  def setup
+    @user_on_generic_trial          = users(:sample_user_on_generic_trial)
+    @user_on_expired_generic_trial  = users(:sample_user_on_expired_generic_trial)
   end
 
-  test "access to websites while not on free trial" do
-    skip
+
+  test "access to websites while on free trial" do
+    sign_in @user_on_generic_trial
+    visit new_website_path
+    assert_text "Add a New Website to Monitor"
+    take_screenshot
+  end
+
+  test "no access to websites while on expired free trial" do
+    sign_in @user_on_expired_generic_trial
+    visit new_website_path
+    assert_text "Please subscribe to access this feature"
+    take_screenshot
   end
 
   test "rendering free trial expiration" do
-    skip
+    sign_in @user_on_generic_trial
+    visit root_path
+    assert_text "Your free trial will end in"
+    take_screenshot
   end
 end
