f7cf802a783e6d6a71bb9051d1611e906ad34be6
Steve Polito <info@stevepolitodesign.com>
2021-02-06 15:46:54 -0500

    Update mailer preview, and account for plain text version.

--- test/mailers/previews/expiring_free_trial_mailer_preview.rb
+++ test/mailers/previews/expiring_free_trial_mailer_preview.rb
@@ -3,7 +3,7 @@ class ExpiringFreeTrialMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/expiring_free_trial_mailer/reminder
   def reminder
-    ExpiringFreeTrialMailer.reminder
+    ExpiringFreeTrialMailer.reminder(user: User.find_by(email: "sample_user_on_generic_trial@example.com"))
   end
 
 end
