05e44b237086e04688fb37c0ffdae8362b17f66a
Steve Polito <info@stevepolitodesign.com>
2021-02-06 16:39:24 -0500

    Update language to account for free trial.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
     def home
-        set_meta_tags  description: "My Site Archive captures screenshots, downloads source code, and monitors DNS records helping you keep track of changes on your websites without needing to restore from a backup. Think of it like the Wayback Machine on steroids."
+        set_meta_tags  description: "My Site Archive captures screenshots, downloads source code, and monitors DNS records helping you keep track of changes on your websites without needing to restore from a backup. Think of it like the Wayback Machine on steroids. Sign up today for a 30 day free trial. No credit card required."
     end
 
     def pricing
@@ -10,7 +10,7 @@ class StaticPagesController < ApplicationController
     end
 
     def features
-        set_meta_tags  description: "My Site Archive captures screenshots, downloads source code, and monitors DNS records helping you keep track of changes on your websites without needing to restore from a backup. Think of it like the Wayback Machine on steroids."
+        set_meta_tags  description: "My Site Archive captures screenshots, downloads source code, and monitors DNS records helping you keep track of changes on your websites without needing to restore from a backup. Think of it like the Wayback Machine on steroids. Sign up today for a 30 day free trial. No credit card required."
     end
 
     def terms_of_use
