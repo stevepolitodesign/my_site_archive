f553f863b1746d2913e678cf8a9a7ae1e2ca14bf
Steve Polito <info@stevepolitodesign.com>
2021-04-08 08:50:34 -0400

    WIP: Add Trix extensions, and style Trix content.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,3 +26,4 @@ document.addEventListener("turbolinks:load", function() {
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import "./subscriptions";
+import "./richtext";
