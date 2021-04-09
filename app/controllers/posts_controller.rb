fb53409b6a01450f58eb66cf34bfe5ba12f61d7b
Steve Polito <stevepolitodesign@users.noreply.github.com>
2021-04-09 08:39:40 -0400

    SEO optimizations (#92)
    * Add posts to footer.
    
    * Add byline to posts#show
    
    * Add updated_at to posts#index.
    
    * bundle add schema_dot_org
    
    * bundle remove schema_dot_org
    
    * WIP: Add structured data.
    
    * Display structured data for blog posts.
    
    * Output schema data as raw.
    
    * WIP: Configure AMP.
    
    * WIP: Render content as plain text to avoid AMP errors.
    
    * WIP: Strip images from posts when view as AMP
    
    * I need to figure out how to update the markup to work with AMP.
    
    * Add container to AMP layout, and style byline.
    
    * Add featured image to posts#show.
    
    * Add featured image to posts#index
    
    * Ignore .DS_Store
    
    * Add AMP schema data.
    
    * Conditionally load featured image in schema data.
    
    * Use amp-img. Conditionally add featured image to schema data.
    
    * Create RSS.
    
    * Cache posts#show and posts#index.
    
    * WIP: Add navigation to AMP layout.
    
    * WIP: Extend Trix editor.
    
    * https://github.com/lazaronixon/trix-extensions
    
    * WIP: Add Trix extensions, and style Trix content.
    
    * Add schema data to static pages.
    
    * Conditionally render canonical URLs.
    
    * Create separate partials for AMP layout.
    
    * Needed to use different classes.
    
    Co-authored-by: Steve Polito <info@stevepolitodesign.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,51 @@
 class PostsController < ApplicationController
+    include ActionView::Helpers::AssetUrlHelper
+    include Schemable
+
     before_action :set_post, only: [:show, :edit, :update, :destroy]
     before_action :authenticate_user!, except: [:show, :index]
 
     def index
-        @posts = policy_scope Post.all.includes([:rich_text_content]).order(created_at: :desc)
+        @posts = policy_scope Post.all.includes([:rich_text_content, :featured_image_attachment]).order(created_at: :desc)
+        fresh_when @posts
+        respond_to do |format|
+            format.html
+            format.rss { render layout: false }
+        end
     end
 
     def show
+        fresh_when @post
         authorize @post
         set_meta_tags   title: @post.title,
                         description: @post.meta_description,
                         site: nil
+        set_schema_dot_org(schema: {
+            :@type => "BlogPosting",
+            :mainEntityOfPage => {
+                :@type => "WebPage",
+                :@id => post_url(@post)
+            },
+            :headline => @post.title,
+            :image => ([
+                url_for(@post.featured_image),
+            ] if @post.featured_image.attached?),
+            :datePublished => @post.created_at,
+            :dateModified => @post.updated_at,
+            :author => {
+                :@type => "Person",
+                :name => "Steve Polito",
+
+            },
+            :publisher => {
+                :@type => "Organization",
+                :name => "My Site Archive",
+                :logo => {
+                    :@type => "ImageObject",
+                    :url => asset_url("assets/amp_logo.png")
+                }
+            }
+        })
     end
 
     def new
@@ -50,7 +85,7 @@ class PostsController < ApplicationController
     private
 
         def post_params
-            params.require(:post).permit(:title, :meta_description, :slug, :status, :content)
+            params.require(:post).permit(:title, :meta_description, :slug, :status, :content, :featured_image)
         end
 
         def set_post
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
-    include Pagy::Frontend
+  include Pagy::Frontend
+
+  def gravatar_path(email:)
+    hash = Digest::MD5.hexdigest(email)
+    "https://www.gravatar.com/avatar/#{hash}"
+  end
+
+  def is_blog_post?
+    params[:controller] == "posts" && params[:action] == "show"
+  end
+
 end
