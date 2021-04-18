b86721f16e6b70f7db40f68a134ceb87582d9113
Steve Polito <info@stevepolitodesign.com>
2021-04-18 13:36:44 -0400

    Add notes.

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -4,6 +4,7 @@ class Archive
 
   attr_accessor :url, :user_id
 
+  # TODO: Consider adding a validation to prevent anonymous users from running too many reports
   validates :user_id, :url, presence: true
   validates :user_id, numericality: { only_integer: true, greater_than: 0 }
   validates :url, url: true
