d0efc482086480a4f33dd17d9bbdaa9d5ab732bb
Steve Polito <info@stevepolitodesign.com>
2021-04-25 15:13:51 -0400

    Correctly save the entered URL.

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -9,13 +9,14 @@ class Archive < ApplicationRecord
   validates :url, url: true
 
   def generate_report
-    # TODO: Extract this into a Job.
+    # TODO: Extract this into a Job. The job should have a low prioritiy.
     ActiveRecord::Base.transaction do
-      domain = remove_path_from_url      
-      @website = self.build_website(url: domain, title: domain, user_id: user_id)
+      original_url  = url
+      domain        = remove_path_from_url      
+      @website      = self.build_website(url: domain, title: domain, user_id: user_id)
       @website.save!(validate: false)
       @website.capture_new_zone_file
-      @webpage = @website.webpages.create(url: url, title: url)
+      @webpage      = @website.webpages.create(url: original_url, title: original_url)
       @webpage.capture_new_screenshot
       @website
     end
