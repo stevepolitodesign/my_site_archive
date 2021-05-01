158c4012246f7d90789d7e7fd93f5e2dc63a7066
Steve Polito <info@stevepolitodesign.com>
2021-05-01 08:21:47 -0400

    Revert "WIP: Add hcaptcha."
    This reverts commit 5eb9a40bc379dc6d61f51fe791cd2f5b51273800.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,13 +6,8 @@ module ApplicationHelper
     "https://www.gravatar.com/avatar/#{hash}"
   end
 
-  def is_archive_generator?
-    params[:controller] == "archives" && params[:action] == "new"
-  end
-
   def is_blog_post?
     params[:controller] == "posts" && params[:action] == "show"
   end
-
 
 end
--- app/views/shared/_hcaptcha_script.html.erb
+++
@@ -1,1 +0,0 @@
-<script src="https://hcaptcha.com/1/api.js" async defer></script>
