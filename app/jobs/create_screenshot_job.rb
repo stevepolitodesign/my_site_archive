7c7ab78286271753eb6b66991e418c3211bc4dc2
Steve Polito <info@stevepolitodesign.com>
2021-05-12 22:43:59 -0400

    WIP: Pseudo code.

--- app/jobs/create_screenshot_job.rb
+++ app/jobs/create_screenshot_job.rb
@@ -14,8 +14,6 @@ class CreateScreenshotJob < ApplicationJob
     
     # OPTIMIZE: This can probaby be extracted into Browserless::BaseJob, and merged with capture_screenshot_and_attach_image.
     def capture_and_create_screenshot_and_html_document(url)
-      # TODO: Use Browslerless API 
-      # https://docs.browserless.io/docs/content.html
       uri = URI("https://chrome.browserless.io/screenshot?token=#{Rails.application.credentials.dig(:browserless, :private_key) }")
       res = Net::HTTP.post(
         uri,
@@ -31,12 +29,10 @@ class CreateScreenshotJob < ApplicationJob
           "Cache-Control" => "no-cache"
         }
       )
-      byebug
-      puts res.body
-      # TODO:
-      # @browserless = Browserless.new(markup: markup, screenshot: screenshot)
+      # TODO: I need to convert the response to an actual PNG 
+      image = res.body
       @screenshot = @webpage.screenshots.build
-      @screenshot.image.attach(io: File.open(screenshot), filename: file_name.split("/").last)
+      @screenshot.image.attach(io: File.open(image), filename: "some-image.png")
       @screenshot.create_html_document(source_code: markup) if @screenshot.save
       TmpFileRemover.new(file_name).call
       @screenshot
