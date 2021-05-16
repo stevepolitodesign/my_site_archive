5abc9a862a17acebddc3e50ca2e6f4c09a4ab723
Steve Polito <info@stevepolitodesign.com>
2021-05-16 08:43:25 -0400

    Write failing test.

---
+++ test/system/html_document_flows_test.rb
@@ -0,0 +1,7 @@
+require "application_system_test_case"
+
+class HtmlDocumentFlowsTest < ApplicationSystemTestCase
+  test "preventing a user from viewing another's html_document" do
+    fail
+  end
+end
