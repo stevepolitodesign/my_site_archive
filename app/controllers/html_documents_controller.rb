af72e3085a86d5c60ad4c9e4f50635f210495de6
Steve Polito <info@stevepolitodesign.com>
2021-05-16 07:58:47 -0400

    Render source code as raw HTML

--- app/controllers/html_documents_controller.rb
+++ app/controllers/html_documents_controller.rb
@@ -1,9 +1,12 @@
 class HtmlDocumentsController < ApplicationController
+  layout :false
+
   before_action :set_website
   before_action :set_html_document
 
   def show
     authorize @website
+    render plain: @html_document.source_code.html_safe
   end
 
   private
