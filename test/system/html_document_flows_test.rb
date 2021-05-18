8e9a3270acf6b4cf7f31d6351c2337b2b09fb6c7
Steve Polito <info@stevepolitodesign.com>
2021-05-17 22:57:08 -0400

    Delete obsolete test.

--- test/system/html_document_flows_test.rb
+++
@@ -1,7 +0,0 @@
-require "application_system_test_case"
-
-class HtmlDocumentFlowsTest < ApplicationSystemTestCase
-  test "preventing a user from viewing another's html_document" do
-    fail
-  end
-end
