f45d36dc83917bd02e49db3ec9ebd32642e81664
Steve Polito <info@stevepolitodesign.com>
2021-05-19 06:45:20 -0400

    Rollback migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_102525) do
+ActiveRecord::Schema.define(version: 2021_01_08_002828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,7 +134,6 @@ ActiveRecord::Schema.define(version: 2021_05_19_102525) do
     t.bigint "webpage_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "width", default: 1024
     t.index ["webpage_id"], name: "index_screenshots_on_webpage_id"
   end
 
