1251b09ff497688c099bd8d21db20f027e1f614c
Steve Polito <info@stevepolitodesign.com>
2021-05-19 22:58:14 -0400

    Create smoke test.

--- test/models/stat_test.rb
+++ test/models/stat_test.rb
@@ -2,9 +2,11 @@ require 'test_helper'
 
 class StatTest < ActiveSupport::TestCase
   setup do
+    @screenshot = screenshots(:one)
+    @stat = @screenshot.build_stat
   end
 
   test "should be valid" do
-    fail
+    assert @stat.valid?
   end
 end
