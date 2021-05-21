47d9d47e873367ba1217fe1ec2bb3edd42278d38
Steve Polito <info@stevepolitodesign.com>
2021-05-20 21:34:07 -0400

    Update test.

--- test/integration/archive_flow_test.rb
+++ test/integration/archive_flow_test.rb
@@ -9,21 +9,21 @@ class ArchiveFlowTest < ActionDispatch::IntegrationTest
     VCR.eject_cassette
   end
 
-  test "archive a weebsite" do
+  test "archive a website" do
     get "/free-website-archive-tool"
     assert_response :success
     assert_select "form"
     assert_difference(["Archive.count","Website.count", "Webpage.count", "Screenshot.count", "ZoneFile.count", "HtmlDocument.count", "Stat.count"], 1) do
       post archives_path, params: {
         archive: {
-          url: "http://example.com/foo.html"
+          url: "https://www.google.com/"
         }
       }
       perform_enqueued_jobs
     end
     assert_redirected_to Archive.last
     follow_redirect!
-    assert_match "http://example.com/foo.html", @response.body
+    assert_match "https://www.google.com/", @response.body
   end
 
   test "can view source code" do
