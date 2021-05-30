60fa2531319ea0fbcaf1ee786ba86c6fef80b4b8
Steve Polito <stevepolito@hey.com>
2021-05-30 12:19:51 -0400

    Call broadcast_action_to to update page in realtime.

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -11,7 +11,7 @@ class Screenshot < ApplicationRecord
     # TODO: Ensure this is scoped to the current user. I need to make sure the latest screenshot created isn't rendered on the page.
     # It needs to be the user's screenshot.
     def broadcast_later
-      self.broadcast_action_later_to(
+      self.broadcast_action_to(
         [self.webpage.website.user, :screenshots],
         action: :replace,
         target: "screenshots",
