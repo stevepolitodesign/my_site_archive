d5b51d510c71f6decd82a4871e255387f94ba526
Steve Polito <stevepolito@hey.com>
2021-05-30 12:13:45 -0400

    Syntax and formatting.

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -5,14 +5,18 @@ class Screenshot < ApplicationRecord
   has_one_attached :image
 
   after_create_commit :broadcast_later
-
-  # TODO: Ensure this is scoped to the current user. I need to make sure the latest screenshot created isn't rendered on the page.
-  # It needs to be the user's screenshot.
-  # broadcasts_to ->(screenshot) { [ screenshot.webpage.website.user, :screenshots ] }, inserts_by: :replace, partial: "screenshots/screenshot", layout: "screenshots/layouts/column"
-
+  
   private
-
+  
+    # TODO: Ensure this is scoped to the current user. I need to make sure the latest screenshot created isn't rendered on the page.
+    # It needs to be the user's screenshot.
     def broadcast_later
-      self.broadcast_action_later_to [self.webpage.website.user, :screenshots], action: :replace, target: "screenshots", partial: "screenshots/screenshot", layout: "screenshots/layouts/column"
+      self.broadcast_action_later_to(
+        [self.webpage.website.user, :screenshots],
+        action: :replace,
+        target: "screenshots",
+        partial: "screenshots/screenshot",
+        layout: "screenshots/layouts/column",
+      )
     end
 end
