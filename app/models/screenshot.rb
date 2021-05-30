31f711d702aacc6ba093eb141f5a42efbf803f4c
Steve Polito <stevepolito@hey.com>
2021-05-30 11:35:11 -0400

    WIP: Use Turbo to load screenshot on page.

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -3,4 +3,6 @@ class Screenshot < ApplicationRecord
   has_one :html_document, dependent: :destroy
   has_one :stat, dependent: :destroy
   has_one_attached :image
+
+  broadcasts_to ->(screenshot) { [ screenshot.webpage.website.user, :screenshots ] }, inserts_by: :replace
 end
