78e2ade3b9794e27fdabb7fb3763f43f98da68cf
Steve Polito <stevepolito@hey.com>
2021-05-30 11:36:17 -0400

    Add note

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -4,5 +4,7 @@ class Screenshot < ApplicationRecord
   has_one :stat, dependent: :destroy
   has_one_attached :image
 
+  # TODO: Ensure this is scoped to the current user. I need to make sure the latest screenshot created isn't rendered on the page.
+  # It needs to be the user's screenshot.
   broadcasts_to ->(screenshot) { [ screenshot.webpage.website.user, :screenshots ] }, inserts_by: :replace
 end
