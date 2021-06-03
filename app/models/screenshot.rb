9861f8957b31b80f2e25fe994e975b21eb0e67e1
Steve Polito <stevepolito@hey.com>
2021-06-02 21:26:44 -0400

    WIP: Load data on archive page via turbo.

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -11,12 +11,21 @@ class Screenshot < ApplicationRecord
     # TODO: Ensure this is scoped to the current user. I need to make sure the latest screenshot created isn't rendered on the page.
     # It needs to be the user's screenshot.
     def broadcast_later
-      self.broadcast_action_to(
-        [self.webpage.website.user, :screenshots],
-        action: :replace,
-        target: "archive_screenshot",
-        partial: "screenshots/screenshot",
-        layout: "screenshots/layouts/column",
-      )
+      if self.webpage.website.archive.present?
+        self.broadcast_action_to(
+          [self.webpage.website.user, :screenshots],
+          action: :replace,
+          target: "archive_screenshot",
+          partial: "screenshots/screenshot",
+        )
+      else
+        self.broadcast_action_to(
+          [self.webpage.website.user, :screenshots],
+          action: :replace,
+          target: "archive_screenshot",
+          partial: "screenshots/screenshot",
+          layout: "screenshots/layouts/column",
+        )
+      end
     end
 end
