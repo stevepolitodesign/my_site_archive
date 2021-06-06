99c125aa1a5627d1ab895a321273b0ab32266d57
Steve Polito <stevepolito@hey.com>
2021-06-06 14:59:32 -0400

    WIP: Open Graph Images.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -16,7 +16,15 @@ class StaticPagesController < ApplicationController
     end 
 
     def home
-        set_meta_tags  description: "My Site Archive captures screenshots, downloads source code, tracks Google Lighthouse scores and monitors DNS records helping you keep track of changes on your websites without needing to restore from a backup. Think of it like the Wayback Machine on steroids. Sign up today for a 30 day free trial. No credit card required."
+        set_meta_tags(
+            description: "My Site Archive captures screenshots, downloads source code, tracks Google Lighthouse scores and monitors DNS records helping you keep track of changes on your websites without needing to restore from a backup. Think of it like the Wayback Machine on steroids. Sign up today for a 30 day free trial. No credit card required.",
+            og: {
+                image: "https://mugshotbot.com/m/HBQnrzXu",
+            },
+            twitter: {
+                card: "summary_large_image",
+            }
+        )
     end
 
     def pricing
