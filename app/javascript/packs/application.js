3e03da407889bb6eb2ae151de4ffbc6288a1693d
Steve Polito <stevepolitodesign@users.noreply.github.com>
2021-06-11 06:21:34 -0400

    Initialize tooltips through Stimulus (#146)
    * Initialize Tooltips through Stimulus.
    
    This allows Tooltips to work when rendered through a Turbo Stream.
    
    * Spelling.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,25 +12,8 @@ require("@hotwired/turbo-rails")
 
 import 'bootstrap/js/dist/alert'
 import 'bootstrap/js/dist/popover'
-import Tooltip from 'bootstrap/js/dist/tooltip'
-
-document.addEventListener("turbo:load", function(){
-  loadToolTips();
-})
-document.addEventListener("turbo:before-stream-render", function(){
-  loadToolTips();
-})
-
-const loadToolTips = () => {
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new Tooltip(tooltipTriggerEl)
-  })
-}
 
 import "../stylesheets/application";
-
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
