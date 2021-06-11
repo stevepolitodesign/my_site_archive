c58c0caa5e188a5a8f22e95083fe18ae3ca7c200
Steve Polito <stevepolito@hey.com>
2021-06-11 18:20:22 -0400

    Update job to delete guest users who are confirmed.
    They're confirmed because I set it that way to prevent mailers from going out.

--- test/jobs/purge_guest_accounts_job_test.rb
+++ test/jobs/purge_guest_accounts_job_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PurgeGuestAccountsJobTest < ActiveJob::TestCase
   test "should destroy guest user and associated records" do
-    assert_difference(["Archive.count","Website.count", "Webpage.count", "Screenshot.count", "ZoneFile.count", "HtmlDocument.count", "Stat.count"], -1) do
+    assert_difference(["User.count", "Archive.count","Website.count", "Webpage.count", "Screenshot.count", "ZoneFile.count", "HtmlDocument.count", "Stat.count"], -1) do
       PurgeGuestAccountsJob.perform_now
     end
   end
